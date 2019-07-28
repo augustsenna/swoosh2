0e2265e0ea9b444819c841823fc0e4b1cf185790
Augusto Senna <augustsenna@gmail.com>
2019-07-28 13:56:48 +0100

    lesson 6

---
+++ app/src/main/java/com/yoursperfectapps/swooshx/BaseActivity.kt
@@ -0,0 +1,48 @@
+package com.yoursperfectapps.swooshx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class BaseActivity : AppCompatActivity() {
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCyle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+       }
+    }
+}
